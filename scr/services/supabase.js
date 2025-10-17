e3d2178974befb644736a06feab51817e0175baa
engelzu <engelmobile2020@gmail.com>
2025-10-17 10:38:44 -0300

    Delete scr directory

--- scr/services/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// Pega as credenciais do arquivo .env
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-// Cria e exporta o cliente Supabase
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
